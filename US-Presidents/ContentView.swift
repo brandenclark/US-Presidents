b57674d8f606481b770ededbd14fef967e3319b3
Branden Clark <brandenrayclark@gmail.com>
2019-09-05 11:52:23 -0600

    Initial Commit

---
+++ US-Presidents/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  US-Presidents
+//
+//  Created by Branden Clark on 9/5/19.
+//  Copyright © 2019 Branden Clark. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
